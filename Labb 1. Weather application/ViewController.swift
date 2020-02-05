3cdb3b06c73333eb7414f3396fa2ac33186fbcc0
anton <antongottfredsson@gmail.com>
2020-02-05 09:48:11 +0100

    Initial Commit

---
+++ Labb 1. Weather application/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Labb 1. Weather application
+//
+//  Created by Anton Gottfredsson on 2020-02-05.
+//  Copyright © 2020 Anton Gottfredsson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Labb 1. Weather applicationTests/Labb_1__Weather_applicationTests.swift
@@ -0,0 +1,34 @@
+//
+//  Labb_1__Weather_applicationTests.swift
+//  Labb 1. Weather applicationTests
+//
+//  Created by Anton Gottfredsson on 2020-02-05.
+//  Copyright © 2020 Anton Gottfredsson. All rights reserved.
+//
+
+import XCTest
+@testable import Labb_1__Weather_application
+
+class Labb_1__Weather_applicationTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
